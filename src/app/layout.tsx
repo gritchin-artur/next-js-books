128cfbb77a2e982929177a202532105506951093
artur <grit4in0225@icloud.com>
2025-06-18 16:17:32 +0200

    Update content or fix bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link
+      rel="icon"
+      href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📗</text></svg>"
+    />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Books</title>
+    </head>
       <body>
         <Providers>{children}</Providers>
-      </body>
+     </body>
     </html>
   );
 }
